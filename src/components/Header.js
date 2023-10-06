f1ba35e4a828046d5b58208fbe2439e994864cb4
ferasalghazi <82527533+ferasalghazi@users.noreply.github.com>
2023-10-06 20:16:47 +0200

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React , {useEffect} from "react";
 import { useUserContext } from "../Router/UserContext";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
